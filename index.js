c0f8565e74f6617368ccba58cfe68bb2bdd534dd
Aengus <aengus.2000@gmail.com>
2022-07-30 22:21:18 -0400

    commit tree 1bfcace5c568fbfd9a7e2094b4a4a9fae463afab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+    const Hq = 42;
+    if (someValue < 42) {
+        return Hq - someValue
+    }
+    return someValue - Hq;
+}
+function distanceFromHqInFeet (someValue) {
+    return distanceFromHqInBlocks(someValue)*264;
+}
+function distanceTravelledInFeet (start, destination) {
+    if (destination < start) {
+        return (start - destination)*264
+    }
+    return (destination - start)*264
+};
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet (start, destination) < 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet (start, destination) < 2000) {
+        return (distanceTravelledInFeet (start, destination) - 400)*.02
+    }
+    else if (distanceTravelledInFeet (start, destination) < 2500) {
+        return 25
+    }
+    else if (distanceTravelledInFeet (start, destination) > 2500) {
+        return 'cannot travel that far'
+    }
+    
+}
